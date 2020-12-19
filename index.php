3bac77b96f08c920b6802518cda97692ac5b3bab
badchita <47920377+badchita@users.noreply.github.com>
2020-12-19 22:31:45 +0800

    initial push

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Floom - Animation</title>
+    <link rel="stylesheet" href="style.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,500&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div class="splash">
+        <h1 class="fade-in">Bachita</h1>
+    </div>
+    <header>
+        <div class="logo">
+            Bachita
+        </div>
+
+        <nav>
+            <ul class="nav-links">
+                <li><a class="nav-link" href="index.html">HOME</a></li>
+                <li><a class="nav-link" href="https://codepen.io/your-work/?cursor=ZD0wJm89MSZwPTEmdj00NzQwOTk4Nw=="
+                        target="_blank">ACTIVITY</a></li>
+
+                <li><a class="nav-link" href="https://matebook-project.herokuapp.com/" target="_blank">CANVAS 1</a></li>
+                <li><a class="nav-link" href="https://floom-svg.herokuapp.com/" target=_blank"">CANVAS 2</a></li>
+
+            </ul>
+        </nav>
+
+        <div class="icons">
+            <svg width="100%" height="100%" viewBox="0 0 110 89" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink" class="w105">
+                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g id="profile" transform="translate(3.000000, 3.000000)" stroke="#2A3330" stroke-width="4">
+                        <path
+                            d="M30.3634393,46.5910882 C35.4987529,52.9467287 43.2843346,57 52,57 C60.7137881,57 68.4978933,52.9484745 73.6332421,46.5951946 C90.297373,52.4281849 102.277371,64.7873602 103.98008,79.4187491 C104.208836,81.2138104 102.441175,82.8077776 100.025867,82.9674701 C99.8921787,82.9674701 99.7584901,82.9822564 99.6248014,82.9822564 L4.45632069,83 C1.98159557,83 3.29647218e-05,81.4947509 3.29647218e-05,79.6553305 C-0.000542188801,79.5563737 0.00640968023,79.4575157 0.0208289741,79.3596037 C1.74296457,64.768287 13.7074993,52.4195342 30.3634393,46.5910882 Z"
+                            id="Combined-Shape"></path>
+                        <ellipse id="Oval-path" cx="52" cy="28.5" rx="28" ry="28.5"></ellipse>
+                    </g>
+                </g>
+            </svg>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 378.59 283.99"
+                class="w105 ">
+                <g id="Layer_2" data-name="Layer 2">
+                    <g id="Layer_1-2" data-name="Layer 1">
+                        <path id="Rectangle-4"
+                            d="M10.78,85.34a2.82,2.82,0,0,0-1,.2A2.74,2.74,0,0,0,8.2,89.12L82.86,274.25A2.77,2.77,0,0,0,85.44,276H293.15a2.77,2.77,0,0,0,2.58-1.74L370.39,89.12a2.75,2.75,0,0,0-2.58-3.78Z"
+                            fill="none" style="stroke: rgb(43, 51, 48); stroke-width: 16px;"></path>
+                        <path id="Oval-2" d="M286.85,90.18C286.85,44.79,245.29,8,194,8s-92.84,36.79-92.84,82.18"
+                            style="fill: none; stroke: rgb(43, 51, 48); stroke-width: 16px;"></path>
+                    </g>
+                </g>
+            </svg>
+
+            <svg width="100%" height="100%" viewBox="0 0 64 45" class="w105">
+                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                    <g id="van" transform="translate(1.000000, 2.000000)" stroke="#2B3330" fill="" stroke-width="2.5">
+                        <path
+                            d="M7.975,36.396 L2.181,36.396 C1.308,36.396 0.6,35.688 0.6,34.815 L0.6,1.628 C0.6,0.755 1.308,0.047 2.181,0.047 L37.475,0.047 C38.348,0.047 39.056,0.755 39.056,1.628 L39.056,34.815 C39.056,35.688 38.348,36.396 37.475,36.396 L18.511,36.396 M51.587,34.815 L58.943,34.815 C59.858,34.815 60.6,34.107 60.6,33.235 L60.6,19.712 C60.6,19.266 60.402,18.841 60.056,18.541 L49.197,9.149 C48.892,8.885 48.495,8.739 48.083,8.739 L39.056,8.739 L39.056,34.815 L51.587,34.815">
+                        </path>
+                        <path
+                            d="M48.904,30.864 C45.994,30.864 43.636,33.223 43.636,36.132 C43.636,39.042 45.994,41.4 48.904,41.4 C51.813,41.4 54.171,39.042 54.171,36.132 C54.171,33.223 51.813,30.864 48.904,30.864 Z"
+                            id="front-wheel" fill="#FFFFFF"></path>
+                        <path
+                            d="M13.243,30.864 C10.333,30.864 7.975,33.223 7.975,36.132 C7.975,39.042 10.333,41.4 13.243,41.4 C16.152,41.4 18.511,39.042 18.511,36.132 C18.511,33.223 16.152,30.864 13.243,30.864 Z"
+                            id="back-wheel" fill="#FFFFFF"></path>
+                    </g>
+                </g>
+            </svg>
+
+            <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 378.59 283.99"
+                class="w105 ">
+                <g id="Layer_2" data-name="Layer 2">
+                    <g id="Layer_1-2" data-name="Layer 1">
+                        <path id="Rectangle-4"
+                            d="M10.78,85.34a2.82,2.82,0,0,0-1,.2A2.74,2.74,0,0,0,8.2,89.12L82.86,274.25A2.77,2.77,0,0,0,85.44,276H293.15a2.77,2.77,0,0,0,2.58-1.74L370.39,89.12a2.75,2.75,0,0,0-2.58-3.78Z"
+                            fill="none" style="stroke: rgb(43, 51, 48); stroke-width: 16px;"></path>
+                        <path id="Oval-2" d="M286.85,90.18C286.85,44.79,245.29,8,194,8s-92.84,36.79-92.84,82.18"
+                            style="fill: none; stroke: rgb(43, 51, 48); stroke-width: 16px;"></path>
+                    </g>
+                </g>
+            </svg>
+        </div>
+    </header>
+
+    <main>
+        <div class="banner">
+            <div class="inner-banner">
+                <span class="back">
+                    <span>B</span>
+                    <span>A</span>
+                    <span>C</span>
+                    <span>H</span>
+                    <span>I</span>
+                    <span>T</span>
+                    <span>A</span>
+                </span>
+            </div>
+        </div>
+    </main>
+
+    <script>
+        const splash = document.querySelector('.splash');
+        document.addEventListener('DOMContentLoaded', (e) => {
+            setTimeout(() => {
+                splash.classList.add('display-none');
+            }, 2000);
+        })
+
+        const header = document.querySelector('.header');
+        window.onscroll = function () {
+            var top = window.scrollY;
+            console.log(top);
+
+            if (top >= 50) {
+                header.classList.add('active')
+            } else {
+                header.classList.remove('active')
+            }
+        }
+    </script>
+</body>
+
+</html>
